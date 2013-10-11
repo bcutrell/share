20e8a7bbe6e241778d38805aa7af91c4255dc151
Ben Cutrell <bcutrell13@gmail.com>
2013-10-11 17:02:12 -0400

    adds geocoder test

---
+++ spec/features/user_provides_location_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+feature 'User provides the location of a good', %Q{
+  As a user
+  I want to provide a location of a good
+  So that people can find it
+} do
+
+# Acceptance Criteria
+# * I must be logged in 
+# * I must be adding a valid good
+# * I can provide a street address for the good
+# * When someone views the good they can see the location on google maps 
+
+  scenario 'My group has an encrpted password' do 
+  # http://railscasts.com/episodes/273-geocoder
+  end
+end
