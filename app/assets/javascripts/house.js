0ef181fd97947521ca7513cc0e06cc4e4179dfa7
Ben Cutrell <bcutrell13@gmail.com>
2013-10-16 18:06:38 -0400

    adds house stlying

--- app/assets/javascripts/house.js
+++ app/assets/javascripts/house.js
@@ -1,45 +1,45 @@
 
-function house_hover(element, text){
-  $(element).mouseenter(function(){
-  $(element).addClass("on_room");
-  $('<div/>', {
-        class: 'center house_text',
-        text: text
-    }).appendTo(element);
-  // $(element).toggle();
-  });
-  $(element).mouseleave(function(){
-    $(element).removeClass("on_room");
-    $('.house_text').hide();
-  });
-}
+// function house_hover(element, text){
+//   $(element).mouseenter(function(){
+//   $(element).addClass("on_room");
+//   $('<div/>', {
+//         class: 'center house_text',
+//         text: text
+//     }).appendTo(element);
+//   // $(element).toggle();
+//   });
+//   $(element).mouseleave(function(){
+//     $(element).removeClass("on_room");
+//     $('.house_text').hide();
+//   });
+// }
 
-function show_all(element, text){
-  $('#show_button').click(function(){
-  $(element).toggleClass("on_room");
-  });
-}
+// function show_all(element, text){
+//   $('#show_button').click(function(){
+//   $(element).toggleClass("on_room");
+//   });
+// }
 
-var $kitchen = $("#kitchen");
-var $foyer = $("#foyer");
-var $living_room = $("#living_room");
-var $hallway = $("#hallway");
-var $bedroom = $("#bedroom");
-var $storage =  $("#storage");
-var $bathroom =  $("#bathroom");
+// var $kitchen = $("#kitchen");
+// var $foyer = $("#foyer");
+// var $living_room = $("#living_room");
+// var $hallway = $("#hallway");
+// var $bedroom = $("#bedroom");
+// var $storage =  $("#storage");
+// var $bathroom =  $("#bathroom");
 
-house_hover($kitchen, "Kitchen");
-house_hover($foyer, "Foyer");
-house_hover($living_room, "Living Room");
-house_hover($hallway, "Hallway");
-house_hover($bedroom, "Bedroom");
-house_hover($storage, "Storage");
-house_hover($bathroom, "Bathroom");
+// house_hover($kitchen, "Kitchen");
+// house_hover($foyer, "Foyer");
+// house_hover($living_room, "Living Room");
+// house_hover($hallway, "Hallway");
+// house_hover($bedroom, "Bedroom");
+// house_hover($storage, "Storage");
+// house_hover($bathroom, "Bathroom");
 
-show_all($kitchen, "Kitchen");
-show_all($foyer, "Foyer");
-show_all($living_room, "Living Room");
-show_all($hallway, "Hallway");
-show_all($bedroom, "Bedroom");
-show_all($storage, "Storage");
-show_all($bathroom, "Bathroom");
+// show_all($kitchen, "Kitchen");
+// show_all($foyer, "Foyer");
+// show_all($living_room, "Living Room");
+// show_all($hallway, "Hallway");
+// show_all($bedroom, "Bedroom");
+// show_all($storage, "Storage");
+// show_all($bathroom, "Bathroom");
